1eea8c500e66ccc1f50b9696a2dd6f11a7601644
symarys <symarys@gmail.com>
2015-09-11 09:18:13 +0200

    fix for travis

--- init.rb
+++ init.rb
@@ -9,9 +9,10 @@ Redmine::Plugin.register :redmine_overwriting_roles do
   permission :manage_roles, { :project_roles => [:index, :edit, :reset, :save]}
   settings :default => {'permissions' => []}, :partial => 'settings/project_role_settings'
 
+  require 'redmine_overwriting_roles/patches/role_patch'
+
   ActionDispatch::Callbacks.to_prepare do
     require 'redmine_overwriting_roles/patches/projects_helper_patch'
-    require 'redmine_overwriting_roles/patches/role_patch'
     require 'redmine_overwriting_roles/patches/user_patch'
   end
 end
